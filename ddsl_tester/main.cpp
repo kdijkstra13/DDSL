47dfa3d6464e01f2b84ecc972e8cd4571caa35f5
Klaas Dijkstra <k.dijkstra@nhl.nl>
2017-05-15 16:15:23 +0200

    PSNR Calculation

--- ddsl_tester/main.cpp
+++ ddsl_tester/main.cpp
@@ -12,48 +12,19 @@ using namespace DSLang;
 using namespace DSModel;
 using namespace DSImage;
 
-//Peak Signal To Noise Ratio
-template <typename T, typename TIdx>
-T PSNR(const Matrix<T, TIdx> &a, const Matrix<T, TIdx> &b) {
-
-}
-
-//Structural Simularity Index
-template <typename T, typename TIdx>
-T SSIM(const Matrix<T, TIdx> &a, const Matrix<T, TIdx> &b, Matrix<T, TIdx> &dst, T max = numeric_limits<T>().max()) {
-	//Wang et al. 2004
-	const T sigma = (T)1.5;
-	const TIdx w_w=11;
-	const TIdx w_h=11;
-	const TIdx w_ox=5; 
-	const TIdx w_oy=5;
-	const T K1 = (T)0.01;
-	const T K2 = (T)0.03;
-	const T L = max;
-	const T C1 = (K1 * L) * (K1 * L);
-	const T C2 = (K2 * L) * (K2 * L);
-	const T C3 = (C2 / T(2));
-
-	auto aggr = makeAggrSSIM(w_h, w_w, w_oy, w_ox, sigma, C1, C2, C3);
-	auto func = makeFuncLinear(T(1), T(0));
-	return slide<T, TIdx, w_h, w_w, w_ox, w_oy>(a, b, dst, aggr, func);
-}
 
 int main(void) {
 	try {		
-		Matrix<Float> m =ImagePNG<Float>("c:/temp/bear.png", true).getChannel(1);
+		Matrix<Float> a = ImagePNG<Float>("c:/temp/simul/original.png", true).getChannel(1);
+		Matrix<Float> b = ImagePNG<Float>("c:/temp/simul/upsampled.png", true).getChannel(1);
 
-		Matrix<Float> m2;
-		auto aggr = makeAggrGauss(11, 11, 5, 5, 1.5f);
-		auto func = makeFuncLinear(1.0f, 1.0f);
-		slide<Float, UInt32, 11, 11, 5, 5>(m, m2, aggr, func);
 
-		//Matrix<Float, UInt32> m2 = randomReal(m.rows.count(), m.cols.count(), 0.0f, 255.0f);
-		Matrix<Float> m3;
-		cout << SSIM(m, m2, m3, 255.0f);
+		Matrix<Float> dst;
+		cout << SSIM(a, b, dst, 255.0f) << endl;
+		cout << PSNR(a, b, 255.0f) << endl;
 
-		ImagePNG<Float>("c:/temp/bear2.png", m3 * 255.0f).saveImage();
-		ImagePNG<Float>("c:/temp/bear3.png", m2).saveImage();
+		ImagePNG<Float>("c:/temp/simul/SSIM.png", dst * 100.0f).saveImage();
+
 	} catch (const double &e) {
 		cout << "double: " << e << endl;
 	} catch (int &e) {
--- src/h/lib/DS_ImageFunc.h
+++ src/h/lib/DS_ImageFunc.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "h/lib/DS_Image.h"
 
 namespace DSFunc {
 	//********************//
@@ -39,5 +40,93 @@ namespace DSFunc {
 
 	template <typename T, typename TIdx>
 	Matrix<T, TIdx> tilesToTiled(const Matrix<Matrix<T, TIdx>, TIdx> &tiles, const TIdx tileCountX, const TIdx tileCountY);
+	
+	//**
+	//** Aggregation functions (note: Use helper functions to use them)
+	//**
+
+	template <typename T, typename TIdx>
+	T aggrMean(const T * const pixelPtr, const TIdx windowHeight, const TIdx windowWidth, const TIdx windowOriginY, const TIdx windowOriginX, const TIdx imgStrideY, const TIdx imgStrideX);
+
+	template <typename T, typename TIdx>
+	T aggrSum(const T * const pixelPtr, const TIdx windowHeight, const TIdx windowWidth, const TIdx windowOriginY, const TIdx windowOriginX, const TIdx imgStrideY, const TIdx imgStrideX);
+
+	template <typename T, typename TIdx>
+	T aggrMin(const T * const pixelPtr, const TIdx windowHeight, const TIdx windowWidth, const TIdx windowOriginY, const TIdx windowOriginX, const TIdx imgStrideY, const TIdx imgStrideX);
+
+	template <typename T, typename TIdx>
+	T aggrConvolution(const T * const pixelPtr, const TIdx windowHeight, const TIdx windowWidth, const TIdx windowOriginY, const TIdx windowOriginX, const TIdx imgStrideY, const TIdx imgStrideX, const DSLib::Matrix<T, TIdx> &mask);
+
+	template <typename T, typename TIdx>
+	T aggrSSIM(const T * const pixelPtr1, const T * const pixelPtr2, const TIdx windowHeight, const TIdx windowWidth, const TIdx windowOriginY, const TIdx windowOriginX, const TIdx imgStrideY1, const TIdx imgStrideX1, const TIdx imgStrideY2, const TIdx imgStrideX2, const DSLib::Matrix<T, TIdx> &mask, const T C1, const T C2, const T C3);
+	
+	//**
+	//Helper functions for creating lambdas
+	//**
+
+	template <typename T, typename TIdx>
+	void drawGaussian(DSLib::Matrix<T, TIdx> &mat, const TIdx meanY, const TIdx meanX, const T sigma);
+
+	template <typename T, typename TIdx>
+	std::function<T(const T * const, const TIdx, const TIdx)> makeAggrMean(const TIdx windowHeight, const TIdx windowWidth, const TIdx windowOriginY, const TIdx windowOriginX);
+
+	template <typename T, typename TIdx>
+	std::function<T(const T * const, const TIdx, const TIdx)> makeAggrSum(const TIdx windowHeight, const TIdx windowWidth, const TIdx windowOriginY, const TIdx windowOriginX);
+
+	template <typename T, typename TIdx>
+	std::function<T(const T * const, const TIdx, const TIdx)> makeAggrConvolution(const DSLib::Matrix<T, TIdx> &mask, const TIdx windowOriginY, const TIdx windowOriginX);
+
+	template <typename T, typename TIdx>
+	std::function<T(const T * const p1, const TIdx, const TIdx, const T * const, const TIdx, const TIdx)> makeAggrSSIM(const TIdx windowHeight, const TIdx windowWidth, const TIdx windowOriginY, const TIdx windowOriginX, const T sigma, const T C1, const T C2, const T C3);
+
+	template <typename T, typename TIdx>
+	std::function<T(const T * const, const TIdx, const TIdx)> makeAggrGauss(const TIdx windowHeight, const TIdx windowWidth, const TIdx windowOriginY, const TIdx windowOriginX, const T sigma);
+
+	template <typename T, typename TIdx>
+	std::function<T(const T * const, const TIdx, const TIdx)> makeAggrMin(const TIdx windowHeight, const TIdx windowWidth, const TIdx windowOriginY, const TIdx windowOriginX);
+
+	template <typename T>
+	std::function<T(const T)> makeFuncQuadratic(const T a, const T b, const T c);
+
+	template <typename T>
+	std::function<T(const T)> makeFuncLinear(const T a, const T b);
+
+	template <typename T>
+	std::function<T(const T, const T)> makeFuncDiffSq();
+
+	template <typename T>
+	T getSlideArea(const T height, const T width, const T windowHeight, const T windowWidth);
+
+	//Slide two images with function
+	template <typename T, typename TIdx, const TIdx windowHeight, const TIdx windowWidth, const TIdx windowOriginX, const TIdx windowOriginY>
+	T slide(const DSLib::Matrix<T, TIdx> &a, const DSLib::Matrix<T, TIdx> &b, DSLib::Matrix<T, TIdx> &dst, std::function<T(const T, const T)> func);
+
+	//Slide one image with function
+	template <typename T, typename TIdx, const TIdx windowHeight, const TIdx windowWidth, const TIdx windowOriginX, const TIdx windowOriginY>
+	T slide(DSLib::Matrix<T, TIdx> &mat, std::function<T(const T)> func);
+
+	//Slide one image with aggregation and function
+	template <typename T, typename TIdx, const TIdx windowHeight, const TIdx windowWidth, const TIdx windowOriginX, const TIdx windowOriginY>
+	T slide(const DSLib::Matrix<T, TIdx> &src, DSLib::Matrix<T, TIdx> &dst, std::function<T(const T * const, const TIdx, const TIdx)> aggr, std::function<T(const T)> func);
+
+	//Slide two images with aggregation and function
+	template <typename T, typename TIdx, const TIdx windowHeight, const TIdx windowWidth, const TIdx windowOriginX, const TIdx windowOriginY>
+	T slide(const DSLib::Matrix<T, TIdx> &a, const DSLib::Matrix<T, TIdx> &b, DSLib::Matrix<T, TIdx> &dst, std::function<T(const T * const p1, const TIdx, const TIdx, const T * const, const TIdx, const TIdx)> aggr, std::function<T(const T)> func);
+	
+	//*********************//
+	//** Image functions **//
+	//*********************//
+
+	//Structural Simularity Index
+	template <typename T, typename TIdx>
+	T SSIM(const Matrix<T, TIdx> &a, const Matrix<T, TIdx> &b, Matrix<T, TIdx> &dst, T max = numeric_limits<T>().max());
+
+	//Mean Squared Error
+	template <typename T, typename TIdx>
+	T MSE(const Matrix<T, TIdx> &a, const Matrix<T, TIdx> &b);
+
+	//Peak signal-to-noise Index
+	template <typename T, typename TIdx>
+	T PSNR(const Matrix<T, TIdx> &a, const Matrix<T, TIdx> &b, T max = numeric_limits<T>().max());
 
 }
