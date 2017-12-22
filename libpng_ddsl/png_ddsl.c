ca991e606a00fdf11e0799301e7ff7c61aa2a8a8
Unknown <coen_koomen@hotmail.com>
2017-12-22 10:54:31 +0100

    replaced png_destroy_read_struct with write struct
    Also changed some hardcoded paths

--- libpng_ddsl/png_ddsl.c
+++ libpng_ddsl/png_ddsl.c
@@ -73,7 +73,7 @@ int writeRawPNG(void * src, unsigned int h, unsigned int w, unsigned char c, uns
 
 	png_write_end(png_ptr, NULL);	
 	//free(png_ptr);
-	png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+    png_destroy_write_struct(&png_ptr, &info_ptr);
 	free(info_ptr);
 	fclose(f);
 	return PNG_OK;
